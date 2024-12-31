e454a1ea0860eeae6fd6cad59bd9bdc089260a66
Fatih Kürekçi <fatihkr02@gmail.com>
2025-01-01 01:57:15 +0300

    updated layout with identity

--- PresentationLayer/Controllers/DashboardController.cs
+++ PresentationLayer/Controllers/DashboardController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationLayer.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DashboardController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
+            //var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            //var values = _articleService.TGetArticlesByAppUserId(userValue.Id);
             return View();
         }
     }
